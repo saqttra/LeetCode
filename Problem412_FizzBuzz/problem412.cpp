146c9925a372d7b431265ae4ccefc056fec90042
saqttra <113034629+saqttra@users.noreply.github.com>
2024-06-12 21:54:14 -0600

    Add 412. Fizz Buzz

---
+++ Problem412_FizzBuzz/problem412.cpp
@@ -0,0 +1,67 @@
+/*
+
+412. Fizz Buzz
+
+Given an integer n, return a string array answer (1-indexed) where:
+
+    answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+    answer[i] == "Fizz" if i is divisible by 3.
+    answer[i] == "Buzz" if i is divisible by 5.
+    answer[i] == i (as a string) if none of the above conditions are true.
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+
+Constraints:
+
+    1 <= n <= 10^4
+*/
+
+#include <vector>
+#include <iostream>
+using std::string;
+using std::vector;
+
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer(n);
+        for (int i = 1; i <= n; i++){
+            if (i % 3 == 0 && i % 5 == 0) {
+                answer[i - 1] = "FizzBuzz";
+            } else if (i % 3 == 0) {
+                answer[i - 1] = "Fizz";
+            } else if (i % 5 == 0) {
+                answer[i - 1] = "Buzz";
+            } else {
+                answer[i - 1] = std::to_string(i);
+            }
+        }
+        return answer;
+    }
+};
+
+
+int main(){
+    Solution p412;
+    int n;
+    std::cin >> n;
+
+    for (const string& str : p412.fizzBuzz(n)) {
+        std::cout << str << ' ';
+    }
+    std::cout << '\n';
+    return 0;
+}
